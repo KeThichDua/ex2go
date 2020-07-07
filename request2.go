4fcacb7a68637f6684fb91f7639c2cb13a590b99
Lam <buihoanglam99@gmail.com>
2020-07-07 17:42:49 +0700

    repair request 2,5

--- request2.go
+++ request2.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func Run2() {
 	// 2. Viết 1 đoạn chương trình tính ra ngày hiện tại theo timestamp.
 	// Điểm mốc từ mức 0  tại 1/1/1970
 	fmt.Println("\n	2.")
-	fmt.Println("Ngày hiện tại: ", time.Now())
+	fmt.Println("Số ngày hiện tại: ", 1592190385/3600/24)
 }
--- request5.go
+++ request5.go
@@ -8,6 +8,7 @@ import (
 func Run5() {
 	//5. Tính ra số ngày trong tuần(dạng string và number) của mốc thời gian sau `1592190385`
 	fmt.Println("\n	5.")
-	fmt.Println("Số ngày: ", 1592190385/3600/24)
-	fmt.Println("Ngày này là: ", time.Unix(1592190385, 0).Weekday())
+	weekday := time.Now().Weekday()
+	fmt.Println("Số ngày trong tuần là: ", int(weekday))
+	fmt.Println("Ngày này là: ", weekday)
 }
