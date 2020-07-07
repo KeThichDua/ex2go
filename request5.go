3748fc22560c18f276d97898be98fb19ef535d07
Lam <buihoanglam99@gmail.com>
2020-07-07 17:28:53 +0700

    repair request 5

--- request5.go
+++ request5.go
@@ -5,27 +5,6 @@ import (
 	"time"
 )
 
-func CheckDay(i int) {
-	switch i {
-	case 1:
-		fmt.Println("Thứ hai")
-	case 2:
-		fmt.Println("Thứ ba")
-	case 3:
-		fmt.Println("Thứ tư")
-	case 4:
-		fmt.Println("Thứ năm")
-	case 5:
-		fmt.Println("Thứ sáu")
-	case 6:
-		fmt.Println("Thứ bảy")
-	case 7:
-		fmt.Println("Chủ nhật")
-	default:
-		fmt.Println("Lỗi")
-	}
-}
-
 func Run5() {
 	//5. Tính ra số ngày trong tuần(dạng string và number) của mốc thời gian sau `1592190385`
 	fmt.Println("\n	5.")
